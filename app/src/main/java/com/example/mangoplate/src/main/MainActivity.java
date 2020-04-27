3a95a3c0464f6e6b5b9e561c22cd8cb2b9c77272
Yoondy <ydh0213@gmail.com>
2020-04-28 02:51:10 +0900

    하단 내비게이션 가운데 '+'버튼 눌렀을 때 나타나는 메뉴 구현

--- app/src/main/java/com/example/mangoplate/src/main/MainActivity.java
+++ app/src/main/java/com/example/mangoplate/src/main/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.mangoplate.src.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.mangoplate.R;
 import com.example.mangoplate.src.BaseActivity;
@@ -16,8 +22,14 @@ import com.example.mangoplate.src.main.search_restaurant.SearchRestaurantFragmen
 import com.example.mangoplate.src.main.timeline.TimelineFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import it.sephiroth.android.library.viewrevealanimator.ViewRevealAnimator;
+
 public class MainActivity extends BaseActivity {
     private FragmentManager fragmentManager = getSupportFragmentManager();
+    private ViewRevealAnimator viewRevealAnimator;
+    BottomNavigationView bottomNavigationView;
+    int screenWidth;
+    int screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +39,24 @@ public class MainActivity extends BaseActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.main_frame_layout, SearchRestaurantFragment.newInstance()).commitAllowingStateLoss();
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.main_bot_nav);
+        // screen size
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+
+        viewRevealAnimator = findViewById(R.id.main_anim_center_btn_animator);
+
+        bottomNavigationView = findViewById(R.id.main_bot_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
         bottomNavigationView.setItemIconTintList(null);
+
+        ImageButton btnCloseAddition = findViewById(R.id.main_btn_close_addition);
+        btnCloseAddition.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewRevealAnimator.setDisplayedChild(viewRevealAnimator.getDisplayedChild()+1, true, new Point(screenWidth/2, screenHeight - bottomNavigationView.getHeight()));
+            }
+        });
     }
 
     private class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -51,6 +78,8 @@ public class MainActivity extends BaseActivity {
                     break;
                 }
                 case R.id.item_addition: {
+//                    viewRevealAnimator.showNext();
+                    viewRevealAnimator.setDisplayedChild(viewRevealAnimator.getDisplayedChild()+1, true, new Point(screenWidth/2, screenHeight - bottomNavigationView.getHeight()));
                     break;
                 }
                 case R.id.item_timeline: {
