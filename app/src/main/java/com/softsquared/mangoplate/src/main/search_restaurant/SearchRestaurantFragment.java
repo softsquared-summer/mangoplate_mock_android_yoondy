7906c44e4b23596696a2077cefd5eccb164f2fe9
Yoondy <ydh0213@gmail.com>
2020-04-30 15:44:56 +0900

    맛집찾기 fragment 상단바에 view들 배치

--- app/src/main/java/com/softsquared/mangoplate/src/main/search_restaurant/SearchRestaurantFragment.java
+++ app/src/main/java/com/softsquared/mangoplate/src/main/search_restaurant/SearchRestaurantFragment.java
@@ -23,7 +23,8 @@ public class SearchRestaurantFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_restaurant, container, false);
+        View thisView = inflater.inflate(R.layout.fragment_search_restaurant, container, false);
+
+        return  thisView;
     }
 }
