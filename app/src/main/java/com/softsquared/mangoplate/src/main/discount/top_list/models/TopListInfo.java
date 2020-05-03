92b0403355001d0973ad99a9764a8a73b28ae36d
Yoondy <ydh0213@gmail.com>
2020-05-04 00:18:22 +0900

    할인 Fragment의 TOP 리스트 세팅

---
+++ app/src/main/java/com/softsquared/mangoplate/src/main/discount/top_list/models/TopListInfo.java
@@ -0,0 +1,46 @@
+package com.softsquared.mangoplate.src.main.discount.top_list.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class TopListInfo {
+    // TODO: After completed API 14, These vars and funs must be changed. These are temp names.
+    @SerializedName("imageUrl")
+    private String imageUrl;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("subtitle")
+    private String subtitle;
+
+    @SerializedName("date")
+    private String date;
+
+    @SerializedName("viewCount")
+    private String viewCount;
+
+    @SerializedName("isBookmark")
+    private boolean isBookmark;
+
+    String getImageUrl() { return imageUrl; }
+
+    String getTitle() { return title; }
+
+    String getSubtitle() { return subtitle; }
+
+    String getViewCount() { return viewCount; }
+
+    String getDate() { return date; }
+
+    boolean isBookmark() { return isBookmark; }
+
+    // TODO: test. It must be removed later.
+    public TopListInfo(String imageUrl, String title, String subtitle, String date, String viewCount, boolean isBookmark) {
+        this.imageUrl = imageUrl;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.date = date;
+        this.viewCount = viewCount;
+        this.isBookmark = isBookmark;
+    }
+}
